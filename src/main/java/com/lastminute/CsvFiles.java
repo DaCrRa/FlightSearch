c7d3a4bce8f23ab65f0a3f2f69d57abc6dfff9c4
Dani C R <danielcr86@gmail.com>
2018-11-17 18:00:01 +0100

    Code formatting

--- src/main/java/com/lastminute/CsvFiles.java
+++ src/main/java/com/lastminute/CsvFiles.java
@@ -12,39 +12,39 @@ import static java.util.stream.Collectors.toList;
 
 public final class CsvFiles
 {
-  private CsvFiles()
-  {
-  }
+	private CsvFiles()
+	{
+	}
 
-  public static List<List<String>> readAllRecords(String fileName)
-  {
-    try (Stream<List<String>> records = records(fileName))
-    {
-      return records.collect(toList());
-    }
-  }
+	public static List<List<String>> readAllRecords(String fileName)
+	{
+		try (Stream<List<String>> records = records(fileName))
+		{
+			return records.collect(toList());
+		}
+	}
 
-  public static Stream<List<String>> records(String fileName)
-  {
-    return splitByCommaIgnoringHeader(linesOf(fileName));
-  }
+	public static Stream<List<String>> records(String fileName)
+	{
+		return splitByCommaIgnoringHeader(linesOf(fileName));
+	}
 
-  private static Stream<List<String>> splitByCommaIgnoringHeader(Stream<String> lines)
-  {
-    return lines.skip(1)
-                .map(line -> line.split(","))
-                .map(Arrays::asList);
-  }
+	private static Stream<List<String>> splitByCommaIgnoringHeader(Stream<String> lines)
+	{
+		return lines.skip(1)
+				.map(line -> line.split(","))
+				.map(Arrays::asList);
+	}
 
-  private static Stream<String> linesOf(String fileName)
-  {
-    try
-    {
-      return Files.lines(Paths.get(fileName));
-    }
-    catch (IOException e)
-    {
-      throw new UncheckedIOException("IO error accessing the CSV file " + fileName, e);
-    }
-  }
+	private static Stream<String> linesOf(String fileName)
+	{
+		try
+		{
+			return Files.lines(Paths.get(fileName));
+		}
+		catch (IOException e)
+		{
+			throw new UncheckedIOException("IO error accessing the CSV file " + fileName, e);
+		}
+	}
 }
--- src/test/java/com/lastminute/ReadFlightsTest.java
+++ src/test/java/com/lastminute/ReadFlightsTest.java
@@ -10,45 +10,45 @@ import static org.junit.Assert.assertEquals;
 
 public class ReadFlightsTest
 {
-  private final int flightCount = 89;
+	private final int flightCount = 89;
 
-  @Test
-  public void readFlightPrices()
-  {
-    List<List<String>> prices = readAllRecords(fullPathTo("flight-prices.csv"));
+	@Test
+	public void readFlightPrices()
+	{
+		List<List<String>> prices = readAllRecords(fullPathTo("flight-prices.csv"));
 
-    allFlightsRead(prices);
-    startsWith(prices, asList("IB2818", "186"));
-    endsWith(prices, asList("LH7260", "191"));
-  }
+		allFlightsRead(prices);
+		startsWith(prices, asList("IB2818", "186"));
+		endsWith(prices, asList("LH7260", "191"));
+	}
 
-  @Test
-  public void readFlightRoutes()
-  {
-    List<List<String>> routes = readAllRecords(fullPathTo("flight-routes.csv"));
+	@Test
+	public void readFlightRoutes()
+	{
+		List<List<String>> routes = readAllRecords(fullPathTo("flight-routes.csv"));
 
-    allFlightsRead(routes);
-    startsWith(routes, asList("CPH", "FRA", "IB2818"));
-    endsWith(routes, asList("MAD", "AMS", "LH7260"));
-  }
+		allFlightsRead(routes);
+		startsWith(routes, asList("CPH", "FRA", "IB2818"));
+		endsWith(routes, asList("MAD", "AMS", "LH7260"));
+	}
 
-  private String fullPathTo(String fileName)
-  {
-    return getClass().getClassLoader().getResource(fileName).getPath();
-  }
+	private String fullPathTo(String fileName)
+	{
+		return getClass().getClassLoader().getResource(fileName).getPath();
+	}
 
-  private void allFlightsRead(List<List<String>> flights)
-  {
-    assertEquals(flightCount, flights.size());
-  }
+	private void allFlightsRead(List<List<String>> flights)
+	{
+		assertEquals(flightCount, flights.size());
+	}
 
-  private void startsWith(List<List<String>> actual, List<String> expected)
-  {
-    assertEquals(expected, actual.get(0));
-  }
+	private void startsWith(List<List<String>> actual, List<String> expected)
+	{
+		assertEquals(expected, actual.get(0));
+	}
 
-  private void endsWith(List<List<String>> actual, List<String> expected)
-  {
-    assertEquals(expected, actual.get(actual.size() - 1));
-  }
+	private void endsWith(List<List<String>> actual, List<String> expected)
+	{
+		assertEquals(expected, actual.get(actual.size() - 1));
+	}
 }
