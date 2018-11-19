db64717062c6c2cdc6d4f4fdb5722d53c5d2945f
Dani C R <danielcr86@gmail.com>
2018-11-19 21:21:13 +0100

    FilteredFligthSource.getFlights(FligthFilter)
    * Add the interface FlightFilter
    * Implement the function returning an empty Stream of Flight

--- src/main/java/es/danielcr86/flightSearch/FilteredFlightSource.java
+++ src/main/java/es/danielcr86/flightSearch/FilteredFlightSource.java
@@ -1,11 +1,14 @@
 package es.danielcr86.flightSearch;
 
+import java.util.stream.Stream;
+
 public class FilteredFlightSource {
 
 	public FilteredFlightSource(FlightSource flightSource) {
 		// TODO Auto-generated constructor stub
 	}
 
-
-
+	public Stream<Flight> getFlights(FlightFilter filter) {
+		return Stream.of();
+	}
 }
